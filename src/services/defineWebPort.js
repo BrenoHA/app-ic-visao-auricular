ba1b4cb9c46447ba69892ddf6b77e4ebce428fe0
João Pedro Gama F. Mattos <jopedrogama@gmail.com>
2022-04-18 22:38:55 -0300

    Commit teste- breno + jp

--- src/services/defineWebPort.js
+++ src/services/defineWebPort.js
@@ -1,44 +1,63 @@
 import * as NetWork from 'expo-network';
 
-export const definePort = () =>{
-    console.log("----s--");
-
-    const defaultPort ='';
-    //#TODO ESSA PARTE NÃO ESTÁ RETORNANDO A STRING, E SIM UMA PROMISSE
-    returnPort().then(response => {
-        console.log("RESPONSE:"+ response);
-        defaultPort = response;
-    });
-
-    //#TODO ESSA PARTE DEVERIA ESTAR PRINTANDO O INICIO DA PORTA;
-    console.log("DEFAULT:" + defaultPort);
-    let fullURL='';
-
-    // while(!fullURL){
-        for(let i=0; i>=255; i++){
-            try{
-                let tempURL = `${defaultPort}${i}`;
-                let value = fetch(tempURL)
-                    .then(response => response.json())
-                    .then(data => data.hasOwnProperty("server")).then;
-                if(value){
-                    fullURL=tempURL;
-                    break;
-                }
-            }catch(e){console.log('')};
-        }
-    // }
-    // console.log(fullURL);
-    return fullURL;
+async function getPort() {
+    let valor = await NetWork.getIpAddressAsync().then();
+    return valor;
+    // .then(result =>{
+    //     let strings = result.split('.');
+    //     return strings;
+    //     }
+    // );
 }
 
-function returnPort(){
+const returnPortB = async () => {
     return NetWork.getIpAddressAsync()
     .then(result =>{
         let strings = result.split('.');
         let stringFormat = `http://${strings[0]}.${strings[1]}.${strings[2]}.`;
-        console.log("String:" + stringFormat);
         return stringFormat;
         }
     );
 }
+
+export const definePort = () =>{
+
+    const defaultPort = getPort();
+    
+    console.log("RESPOSTA---JP: "+ defaultPort);
+    
+
+    const defaultPortB = returnPortB().then(response =>{
+        console.log("ESPERADO-B:" +response);
+        return response;
+    });
+    console.log("ESPERADO-B2:" + JSON.stringify(defaultPortB));
+    
+}
+
+console.log('DEFINE PORT RETURN:' + JSON.stringify(definePort()));
+
+
+
+
+
+
+
+
+
+// // while(!fullURL){
+//     for(let i=0; i>=255; i++){
+//         try{
+//             let tempURL = `${defaultPort}${i}`;
+//             let value = fetch(tempURL)
+//                 .then(response => response.json())
+//                 .then(data => data.hasOwnProperty("server")).then;
+//             if(value){
+//                 fullURL=tempURL;
+//                 break;
+//             }
+//         }catch(e){console.log('')};
+//     }
+// // }
+// console.log(`PORTA FINAL: ${fullURL}`);
+// return fullURL;
