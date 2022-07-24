82eee568278c0d2bd154a4dd065e730482cf3bb2
João Pedro Gama F. Mattos <jopedrogama@gmail.com>
2022-07-24 19:05:31 -0300

    definicao de porta pela API

--- src/services/defineWebPort.js
+++ src/services/defineWebPort.js
@@ -1,11 +1,45 @@
 import * as NetWork from 'expo-network';
 import axios from 'axios';
 
-const getPort = async () => {
-  let valor = await NetWork.getIpAddressAsync().then();
-  let strings = valor.split('.');
-  return `http://${strings[0]}.${strings[1]}.${strings[2]}.`;
-};
+// const getPort = async () => {
+//   let valor = await NetWork.getIpAddressAsync().then();
+//   let strings = valor.split('.');
+//   return `http://${strings[0]}.${strings[1]}.${strings[2]}.`;
+// };
+
+// const requestPort = async (url) => {
+//   let valorURL = await axios
+//     .get(url, { timeout: 2000 })
+//     .then((response) => {
+//       return response.data.server;
+//     })
+//     .catch((error) => {});
+
+//   if (valorURL != undefined) {
+//     return valorURL;
+//   }
+// };
+
+// export const returnFullURL = async () => {
+//   const defaultPort = await getPort();
+
+//   let fullURL = defaultPort;
+
+//   // Tries in each port
+//   for (let index = 1; index <= 255; index++) {
+//     let baseURL = `${defaultPort}${index}`;
+//     let tempURL = `${baseURL}/startServer`;
+//     console.log(tempURL);
+
+//     let valorTemp = await requestPort(tempURL);
+//     if (valorTemp != undefined) {
+//       fullURL = valorTemp;
+//       break;
+//     }
+//   }
+//   return fullURL;
+// };
+
 
 const requestPort = async (url) => {
   let valorURL = await axios
@@ -21,21 +55,13 @@ const requestPort = async (url) => {
 };
 
 export const returnFullURL = async () => {
-  const defaultPort = await getPort();
-
-  let fullURL = defaultPort;
-
-  // Tries in each port
-  for (let index = 1; index <= 255; index++) {
-    let baseURL = `${defaultPort}${index}`;
-    let tempURL = `${baseURL}/startServer`;
-    console.log(tempURL);
-
-    let valorTemp = await requestPort(tempURL);
-    if (valorTemp != undefined) {
-      fullURL = valorTemp;
-      break;
+  let browserAgain = true;
+  while(browserAgain){
+    let url = await axios.get("http://jpedrogama.pythonanywhere.com/port").then(response => {return response});
+    let hasResponse = await requestPort(url.data.ip);
+    if(hasResponse){
+      browserAgain=false;
     }
   }
-  return fullURL;
-};
+  return url.ip;
+}
